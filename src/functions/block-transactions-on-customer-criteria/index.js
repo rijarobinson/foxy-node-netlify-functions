ab8abf41c48b09774b47eb0eaf885eb464c38f01
Marija Robinson <marija.robinson@foxycart.com>
2021-10-20 17:58:55 -0500

    testing

--- src/functions/block-transactions-on-customer-criteria/index.js
+++ src/functions/block-transactions-on-customer-criteria/index.js
@@ -19,7 +19,7 @@ const emailsToReject = ["rija@example.com"];
   if (!validation.input.validate(requestEvent)) {
     return validation.input.response();
   }*/
-  const items = extractItems(requestEvent);
+  const items = requestEvent.body;
  /* if (!validation.items.validate(items)) {
     return validation.items.response(items);
   }*/
