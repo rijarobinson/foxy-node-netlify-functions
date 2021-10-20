86e4c84a16863fce4757c899460cfcbfc08de03b
Marija Robinson <marija.robinson@foxycart.com>
2021-10-20 18:07:36 -0500

    just customer email

--- src/functions/block-transactions-on-customer-criteria/index.js
+++ src/functions/block-transactions-on-customer-criteria/index.js
@@ -14,8 +14,8 @@ const emailsToReject = ["rija@example.com"];
  async function handler(requestEvent) {
   // Validation
   // this will be empty if just run in the browser, duh
- //const customerData = extractCustomerDetails(requestEvent.body);
-  const customerData = JSON.stringify(requestEvent.body);
+ const customerData = JSON.stringify(extractCustomerDetails(requestEvent.body));
+ // const customerData = JSON.stringify(requestEvent.body);
 // START HERE!!! just printing stuff. customerData was empty, so not getting body correctly?
 // make changes, commit, then push. Netlify will auto-deploy, then can refresh netlify url in browser to get response
   return {
@@ -75,8 +75,8 @@ const emailsToReject = ["rija@example.com"];
  */
 function extractCustomerDetails(body) {
   const objBody = JSON.parse(body);
-  if (objBody && objBody._embedded && objBody._embedded['fx:customer']) {
-    return objBody._embedded['fx:customer'];
+  if (objBody && objBody._embedded && objBody._embedded['fx:customer']['email']) {
+    return objBody._embedded['fx:customer']['email'];
   }
   return [];
 }
