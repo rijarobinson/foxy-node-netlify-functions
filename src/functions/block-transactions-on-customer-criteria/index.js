58b6cedf3c8c97d8452a722c318621b8fe4597c9
Marija Robinson <marija.robinson@foxycart.com>
2021-10-26 15:56:40 -0500

    specific function for customer email

--- src/functions/block-transactions-on-customer-criteria/index.js
+++ src/functions/block-transactions-on-customer-criteria/index.js
@@ -13,22 +13,16 @@ const FoxyWebhook = require("../../foxy/FoxyWebhook.js");
 
   // Validation
   // this will be empty if just run in the browser, duh
- const customerData = extractCustomerDetails(requestEvent.body);
+ const customerEmail = extractCustomerEmail(requestEvent.body);
   
  const emailsToReject = ["rija@example.com"];
-
-  //const objBody = JSON.parse(requestEvent.body);
-
-  //const customerData = objBody._embedded['fx:customer']['email'];
-
-
 
 // add other strings to reject that maybe aren't emails
 // like just 2nd level domain, or maybe use regex. Not sure how includes would process that
   // make changes, commit, then push. Netlify will auto-deploy, then can refresh netlify url in browser to get response
 //return validCustomer(customerData, emailsToReject); 
 
-if (emailsToReject.includes(customerData)) {
+if (emailsToReject.includes(customerEmail)) {
   return {
     body: JSON.stringify({ details: "problem", ok: false }),
     statusCode: 200,
@@ -43,56 +37,13 @@ return {
 
 }
 
-/*  if (!validation.customer.validate(customerData)) {
-    return validation.customer.response(customerData);
-  }*/
-/*  const values = [];
-  const cache = createCache();
-  // Fetch information needed to validate the cart
-  try {
-    await customerData.reduce(
-      (p, i) => p.then(
-        (accum) => fetchItem(cache, i).then((fetched) => {
-          values.push(fetched);
-          return accum;
-        }),
-      ), Promise.resolve(values),
-    );
-    let failed = findMismatch(values);
-    if (!failed) {
-      const outOfStock = outOfStockItems(values);
-      if (outOfStock) {
-        failed = getMessages().insufficientInventory + " " + outOfStock;
-      }
-    }
-    if (failed) {
-      return {
-        body: JSON.stringify({ details: failed, ok: false, }),
-        statusCode: 200,
-      };
-    } else {
-      console.log('OK: payment approved - no mismatch found')
-      return {
-        body: JSON.stringify({ details: '', ok: true, }),
-        statusCode: 200,
-      };
-    }
-  } catch (e) {
-    console.error(e);
-    return {
-      body: JSON.stringify({ details: "An internal error has occurred", ok: false, }),
-      statusCode: 500,
-    };
-  }*/
-//}
-
 /**
  * Extract Customer Details from payload received from FoxyCart
  *
  * @param {string} body of the data received from datastore or file
  * @returns {Array} an array of items
  */
-function extractCustomerDetails(body) {
+function extractCustomerEmail(body) {
   const objBody = JSON.parse(body);
   if (objBody && objBody._embedded && objBody._embedded['fx:customer']['email']) {
     return objBody._embedded['fx:customer']['email'];
@@ -117,26 +68,6 @@ function extractCustomerDetails(body) {
     body: JSON.stringify({ details: "Ok.", ok: true }),
     statusCode: 200,
   }}*/
-
-/**
- * Validation checks
- */
-/*const validation = {
-  validate: function (requestEvent) {
-    this.errorMessage = FoxyWebhook.validFoxyRequest(requestEvent);
-    return !this.errorMessage;
-  },
-  customer: {
-    response: (body) => ({
-      body: JSON.stringify({
-        details: `Invalid items: ${customer_data.join(',')}`,
-        ok: false,
-      }),
-      statusCode: 200,
-    }),
-    validate: (customer_data) => customer.every(e => validItem(e)),
-  }
-}*/
 
 module.exports = {
   handler
