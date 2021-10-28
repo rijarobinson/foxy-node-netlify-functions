4ad42acaa04cb2251f3381f68aeecd38725fd50a
Marija Robinson <marija.robinson@foxycart.com>
2021-10-28 17:59:20 -0500

    tax endpoint

---
+++ src/functions/custom-tax-endpoint/index.js
@@ -0,0 +1,109 @@
+const FoxyWebhook = require("../../foxy/FoxyWebhook.js");
+const MatchList = require("./matchlist.json");
+
+/**
+ * Receives the request, processes it and sends the response.
+ * 
+ * @param {Object} requestEvent the request event built by Netlify Functions
+ * @returns {Promise<{statusCode: number, body: string}>} the response object
+ */
+
+async function handler(requestEvent) {
+ // const customerEmail = extractCustomerEmail(requestEvent.body);
+ // const customerIP = extractCustomerIP(requestEvent.body);
+
+ // if (getEmailList().includes(customerEmail) || getIPAddressList().includes(customerIP)) {
+ const taxConfiguration =  JSON.stringify({
+  "ok":true,
+  "details":"",
+  "name":"custom tax",
+  "expand_taxes":[
+     {
+        "name":"super tax",
+        "rate":0.045,
+        "amount":0.23
+     },
+     {
+        "name":"mega tax",
+        "rate":0.053,
+        "amount":0.27
+     },
+     {
+        "name":"litle tax",
+        "rate":0.021,
+        "amount":0.11
+     }
+  ],
+  "total_amount":0.6,
+  "total_rate":0.119
+});
+ return taxConfiguration;
+      
+//  }
+ // return {
+//    body: JSON.stringify({ details: "", ok: true }),
+//    statusCode: 200,
+//  }
+//}
+
+/**
+ * Extract Customer Email from payload received from FoxyCart
+ *
+ * @param {string} body of the data received from payload
+ * @returns {string} email address of transaction
+ */
+function extractCustomerEmail(body) {
+  const objBody = JSON.parse(body);
+  if (objBody && objBody._embedded && objBody._embedded['fx:customer']['email']) {
+    return objBody._embedded['fx:customer']['email'];
+  }
+  return "";
+}
+
+/**
+ * Extract Customer IP Address from payload received from FoxyCart
+ *
+ * @param {string} body of the data received from payload
+ * @returns {string} ip address of transaction
+ */
+ function extractCustomerIP(body) {
+  const objBody = JSON.parse(body);
+  if (objBody && objBody['customer_ip']) {
+    return objBody['customer_ip'];
+  }
+  return "";
+}
+
+/**
+ * Opens file and returns list of emails
+ *
+ * @param {Object} email to be validated
+ * @returns {array} array of email addresses
+ */
+
+function getEmailList() {
+  const emailsToReject = MatchList["email_addresses"];
+  if (emailsToReject) {
+    return emailsToReject;
+  }
+  return [];
+}
+
+/**
+ * Opens file and returns IP Addresses to block
+ *
+ * @param {Object} IP Addresses to be validated
+ * @returns {array} array of IP Addresses
+ */
+
+ function getIPAddressList() {
+  const ipsToReject = MatchList["ip_addresses"];
+  if (ipsToReject) {
+    return ipsToReject;
+  }
+  return [];
+}
+
+module.exports = {
+  handler
+}
